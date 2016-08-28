28d023717f88c0e94e0ea135ff1eb6f10a6f95bf
Sudeep Hazra <sudeephazra@gmail.com>
2016-08-28 00:04:22 -0400

    Initial Commit of files

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+'''
+Created on Aug 27, 2016
+
+@author: Sudeep
+'''
+
+#Normal print function
+print ("Hello World")
+
+#print using a custom function
+def print_function(string):
+    print(string)
+    
+print_function("What a day")
+
+#Perform normal numerical functions
+a = 8
+print(a*10) #Multiplication
+print(a**10) #Exponential
+
